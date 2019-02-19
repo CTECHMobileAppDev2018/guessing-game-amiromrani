9bd9c6bab598c7a9bf2f5787064408115c062457
Omrani <1001212@RPS.net>
2019-02-19 09:03:37 -0600

    finishhhh it tonight

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
 
+  
 public class GuessingGame {
 
     public static void main(String[] args) {
+     
     
-        System.out.println("Welcome to the guessing game.");
+        System.out.println("Welcome to the guessing game."); 
+      System.out.println("you have to guess a number from 1 to 100 ");
+      Scanner input = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("Enter your guess");
+      int userinput = input.nextInt();
+      String userName = myObj.nextLine();  // Read user input
+       Random rand = new Random(); 
+  
+        // Generate random integers in range 0 to 100
+        int rand_int = rand.nextInt(101); 
+        if (userinput == rand_int){
+        System.out.println("you got it" );
+     
+        } else if (userinput > rand_int){  
+          System.out.println("your too high");
+          
+        } else {
+            System.out.println("your too low");
+        }   
     
+ }
     }
-
-}
